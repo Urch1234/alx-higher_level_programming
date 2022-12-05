5e563007ef133e0a689de1a84e9735a0128cd814
Urch1234 <iernest61@gmail.com>
2022-12-05 15:44:45 -0800

    13-is_palindrome.c task

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * int is_palindrome - singly linked list
+ * @head: pointer to the head of list
+ * Return: address of the new element or NULL if it fails
+ */
+
+int is_palindrome(listint_t **head)
+{
+
