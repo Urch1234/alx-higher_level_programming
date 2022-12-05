7bd7c1887e06e8869d80aea37bc8b2388cd5fc56
Urch1234 <iernest61@gmail.com>
2022-12-05 12:54:00 -0800

    10-divisible_by_2.py update

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -19,9 +19,9 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-listint_t *addnodeint_end(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 void reverse_listint(listint_t **head);
 int is_palindrome(listint_t **head);
 
-#endif /*LISTS_H*/
+#endif /* LISTS_H */
