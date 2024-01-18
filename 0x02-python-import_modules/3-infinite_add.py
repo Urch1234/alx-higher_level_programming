a4ec19810a7835bf245f7e3ffb3260943f94714f
Ihezie Ernest Uchenna <iernest61@gmail.com>
2024-01-18 06:04:33 -0800

    All task done including Advanced

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """This Python script calculates the summation of all the 
+    command-line arguments passed to it and prints the result.
+    """
+    import sys
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ = "__main__":
+    """
+    This Python script imports the variable a from the 
+    module variable_load_5 and then prints its value
+    """
+    from variable_load_5 import a
+
+    print(a)
