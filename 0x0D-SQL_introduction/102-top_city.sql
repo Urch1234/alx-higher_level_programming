db96ac0618905454b53d11b775daf9f254e66068
Ihezie Ernest Uchenna <iernest61@gmail.com>
2024-03-26 11:10:59 -0700

    Advanced task

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,6 @@
--- Display the top 3 of cities temp during July and August by temperature (DESC).
+-- Displays the 3 cities with the highest average temperatures between July and August.
 SELECT `city`, AVG(`value`) AS `avg_temp`
-FROM `temperature`
+FROM `temperatures`
 WHERE `month` = 7 OR `month` = 8
 GROUP BY `city`
 ORDER BY `avg_temp` DESC
--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,5 +1,5 @@
 -- Display the max temperature of each state, ordered by state.
-SELECT `state`, MAX(`value`) AS `MAX_temp`
+SELECT `state`, MAX(`value`) AS `max_temp`
 FROM `temperatures`
 GROUP BY `state`
 ORDER BY `state`;
