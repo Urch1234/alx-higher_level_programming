f934afcfda0625df775178d423c219d9475f8aae
Ihezie Ernest Uchenna <iernest61@gmail.com>
2024-04-22 07:36:13 -0700

    3-value_argument.js edited

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-  /*
-   * a script that prints the first argument passed to it
-   * If no arguments are passed to the script, print “No argument”
-   * If no arguments are passed to the script, print “No argument”
-   */
+/*
+ * a script that prints the first argument passed to it
+ * If no arguments are passed to the script, print “No argument”
+ * If no arguments are passed to the script, print “No argument”
+ */
   const args = process.argv[2];
   if (args === undefined) {
     console.log('No argument');
