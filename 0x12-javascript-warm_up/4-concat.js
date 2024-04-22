7c37543760f5f7ca2e019e9df9d69a1909f91e49
Ihezie Ernest Uchenna <iernest61@gmail.com>
2024-04-22 08:40:06 -0700

    4-concat.js edited

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
-/*
- * A script that prints two arguments passed to it
- * Using console.log(...) to print all output.
- * var not allowed
- */
 const argv0 = process.argv[2];
 const argv1 = process.argv[3];
 
