769ab125e39ec6e2f32dd347a6265c1b42597279
Ihezie Ernest Uchenna <iernest61@gmail.com>
2024-04-22 11:06:02 -0700

    8-square.js edited

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
-const size = parseInt(process.argv[2]);
+const x = parseInt(process.argv[2]);
 
-if (!size) {
+if (!x) {
 	console.log('Missing size');
 } else {
-		for (let i = 0; i < size; i++) {
-			for (let j = 0; j < size; j++) {
-				console.log('X');
-		}
-		console.log('');
+	for (let i = 0; i < x; i++) {
+		console.log('X'.repeat(x));
 	}
 }
