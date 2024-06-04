702f685c01102649f5d1082f6111f13dd3247797
Ihezie Ernest Uchenna <iernest61@gmail.com>
2024-06-04 07:01:13 -0700

    failing tasks edited

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-const fs = required('fs')
+const fs = require('fs');
 const filename = process.argv[2];
 
-fs.readFile(filename, 'utf-8, (error, content) => {
-				if (error) {
-								console.log(error);
-				} else {
-								console.log(content);
-				}
+fs.readFile(filename, 'utf-8', (error, content) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(content);
+  }
 });
--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-
-const request = required('request');
+const request = require('request');
 const url = process.argv[2];
 
 request.get(url, (error, response) => {
-				if (error) {
-								console.log(error);
-				} else {
-								console.log(`code: ${response.statuscode}`);
-				}
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
 });
